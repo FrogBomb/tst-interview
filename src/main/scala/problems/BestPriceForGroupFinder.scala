f7c85138821f955edc9456b0d30213b86e09e09d
Tom Blanchet <TLBlanchet@gmail.com>
2024-08-18 13:03:05 -0600

    Remove enums - they don't matter

--- src/main/scala/problems/BestPriceForGroupFinder.scala
+++ src/main/scala/problems/BestPriceForGroupFinder.scala
@@ -1,10 +1,6 @@
 package problems
 
 import cats.effect.*
-import RateCode.*
-import RateGroup.*
-import CabinCode.*
-import StrEnumConvertions.given
 
 object BestPriceForGroupFinder extends IOApp.Simple {
   def getBestGroupPrices(
@@ -43,20 +39,20 @@ object BestPriceForGroupFinder extends IOApp.Simple {
 
   def runExample: Seq[BestGroupPrice] =
     val rates = List(
-      Rate(M1, Military),
-      Rate(M2, Military),
-      Rate(S1, Senior),
-      Rate(S2, Senior)
+      Rate("M1", "Military"),
+      Rate("M2", "Military"),
+      Rate("S1", "Senior"),
+      Rate("S2", "Senior")
     )
     val prices = List(
-      CabinPrice(CA, M1, 200.00),
-      CabinPrice(CA, M2, 250.00),
-      CabinPrice(CA, S1, 225.00),
-      CabinPrice(CA, S2, 260.00),
-      CabinPrice(CB, M1, 230.00),
-      CabinPrice(CB, M2, 260.00),
-      CabinPrice(CB, S1, 245.00),
-      CabinPrice(CB, S2, 270.00)
+      CabinPrice("CA", "M1", 200.00),
+      CabinPrice("CA", "M2", 250.00),
+      CabinPrice("CA", "S1", 225.00),
+      CabinPrice("CA", "S2", 260.00),
+      CabinPrice("CB", "M1", 230.00),
+      CabinPrice("CB", "M2", 260.00),
+      CabinPrice("CB", "S1", 245.00),
+      CabinPrice("CB", "S2", 270.00)
     )
 
     getBestGroupPrices(rates, prices)
@@ -85,35 +81,4 @@ case class CabinCodeAndGroup(cabinCode: String, rateGroup: String)
 case class RateCodeAndPrice(rateCode: String, price: BigDecimal) {
   def min(that: RateCodeAndPrice): RateCodeAndPrice =
     if (price > that.price) that else this
-}
-
-// Fancy enumeration for "future" compile-time validation, extended logic on known rate groups and codes, and potentially better type safety of the existing case classes
-// ... I also might just be playing with scala3 features here instead of just interpreting the instructions as string inputs.
-enum RateCode(val value: String) {
-  case M1 extends RateCode("M1")
-  case M2 extends RateCode("M2")
-  case S1 extends RateCode("S1")
-  case S2 extends RateCode("S2")
-  case Other(override val value: String) extends RateCode(value)
-}
-
-enum RateGroup(val value: String) {
-  case Military extends RateGroup("Military")
-  case Senior extends RateGroup("Senior")
-  case Other(override val value: String) extends RateGroup(value)
-}
-
-enum CabinCode(val value: String) {
-  case CA extends CabinCode("CA")
-  case CB extends CabinCode("CB")
-  case Other(override val value: String) extends CabinCode(value)
-}
-
-object StrEnumConvertions {
-  given fromRateCode2str: Conversion[RateCode, String] = (rc: RateCode) =>
-    rc.value
-  given fromRateGroup2str: Conversion[RateGroup, String] = (rg: RateGroup) =>
-    rg.value
-  given fromCabinCode2str: Conversion[CabinCode, String] = (cc: CabinCode) =>
-    cc.value
 }
--- src/test/scala/problems/BestPriceGroupFinderSuite.scala
+++ src/test/scala/problems/BestPriceGroupFinderSuite.scala
@@ -3,10 +3,6 @@ package problems
 import cats.effect.*
 import munit.CatsEffectSuite
 import problems.*
-import RateCode.*
-import RateGroup.*
-import CabinCode.*
-import StrEnumConvertions.given
 
 class BestPriceGroupFinderSuite extends CatsEffectSuite:
 
@@ -17,10 +13,10 @@ class BestPriceGroupFinderSuite extends CatsEffectSuite:
     assertEquals(
       BestPriceForGroupFinder.runExample.toSet,
       Set(
-        BestGroupPrice(CA, M1, 200.00, Military),
-        BestGroupPrice(CA, S1, 225.00, Senior),
-        BestGroupPrice(CB, M1, 230.00, Military),
-        BestGroupPrice(CB, S1, 245.00, Senior)
+        BestGroupPrice("CA", "M1", 200.00, "Military"),
+        BestGroupPrice("CA", "S1", 225.00, "Senior"),
+        BestGroupPrice("CB", "M1", 230.00, "Military"),
+        BestGroupPrice("CB", "S1", 245.00, "Senior")
       )
     )
 
